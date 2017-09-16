d6b40333487371fe7b8327427e15850e3ac17bc8
pyksoft <pyksoft@gmail.com>
2017-09-16 21:56:35 +1000

    Add profile validation on account login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,23 @@ class HomeController < ApplicationController
     def index
         # Get user profile
         @profiles = Profile.all
-        
+
         # Since user can only have one profile at a time
-        @profile = @profiles.first
+        @profile = @profiles.detect { |p|
+            p.account_id == current_account.id
+        }
+
+        puts "current_account: #{current_account.id}"
+
+        # Get friends
+        @friends = Friend.all
+        @friends_count = @friends.count
 
         # Check if user's profile exists, if not create one
-        if @profiles.size == 0
+        if @profile.nil?
             redirect_to new_profile_path
         end
+
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916083349) do
+ActiveRecord::Schema.define(version: 20170916113127) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170916083349) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "friend_account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
