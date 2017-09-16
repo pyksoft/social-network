70004b0d24bc167ea8475b676795ae1e39eb82d5
pyksoft <pyksoft@gmail.com>
2017-09-16 18:43:25 +1000

    Add profile elements to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
     def index
         # Get user profile
         @profiles = Profile.all
+        
+        # Since user can only have one profile at a time
+        @profile = @profiles.first
 
         # Check if user's profile exists, if not create one
         if @profiles.size == 0
